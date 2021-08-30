a056b8f8373c4a811cf635a29bbdc30a84139d32
exersonz <86198455+exersonz@users.noreply.github.com>
2021-08-29 19:26:14 -0700

    Add files via upload

---
+++ js/Computer.js
@@ -0,0 +1,50 @@
+class Computer {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+  
+
+    World.add(world, this.body);
+  }
+
+ 
+  
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+
+  life()
+  {
+    push();
+    textSize(20);
+    fill("white");
+    text("Computer", 950,30);
+
+    fill (this.life1);
+    rect(840,40,70,30);
+    fill (this.life2);
+    rect(910,40,70,30);
+    fill (this.life3);
+    rect(980,40,70,30);
+    pop();
+  }
+}
